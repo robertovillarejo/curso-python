8eb12174afd7f175b0006ebcf99fda17c41709fb
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2024-08-13 19:52:08 -0600

    corrige renombrado de imágenes

--- renombrar-imagenes.py
+++ renombrar-imagenes.py
@@ -11,5 +11,5 @@ for archivo in os.listdir(carpeta):
     if archivo.endswith((".jpg")):
         # renombrar las imágenes con el nombre de la carpeta y un número consecutivo
         os.path.join(carpeta, archivo)
-        os.rename(carpeta + "/" + archivo, carpeta + str(n) + ".jpg")
+        os.rename(carpeta + "/" + archivo, carpeta + "/" + carpeta + str(n) + ".jpg")
         n = n + 1
