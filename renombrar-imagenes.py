6969e03ae5a78d3a445c58a89685ab6d9d468a52
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2024-09-02 18:50:25 -0600

    elimina código comentado

--- renombrar-imagenes.py
+++ renombrar-imagenes.py
@@ -20,21 +20,3 @@ for archivo in os.listdir(carpeta):
         dir_fin = (os.path.join(carpeta,carpeta) + str(n) + extension[-1])
         os.rename(dir_ini, dir_fin)
         n = n + 1
-
-
-# encontrar las imágenes de la carpeta
-# y guardarlas en una lista
-"""
-n = 1
-for archivo in os.listdir(carpeta):
-    if archivo.endswith((".jpg",".JPG",".jpeg",".JPEG",".png",".PNG")):
-        # determinar la extensión del archivo
-        extension = os.path.splitext(archivo)
-        # renombrar las imágenes con el nombre de la carpeta y un número consecutivo
-        os.path.join(carpeta, archivo)
-        os.rename(carpeta + "/" + archivo, carpeta + "/" + carpeta + str(n) + extension[-1])
-        n = n + 1
-"""
-
-
-
