f9e043fc6106b7356c99b9dad82e30b9af1cde8d
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2024-09-03 20:43:13 -0600

    corrige main.py

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-# programa de muestra
-nombre = input("¿Cual es tu nombre? ")
-print("Hola", nombre)
+import flet as ft
+import renombrar_imagenes
+
+def main(page: ft.Page):
+    def pick_files_result(e: ft.FilePickerResultEvent):
+        # se actualiza el texto del cuadro de texto
+        if (e.path != None):
+            carpeta_seleccionada.value = e.path
+        else:
+            carpeta_seleccionada.value = "Cancelado"
+        carpeta_seleccionada.update()
+
+    # se crea un selector de archivos
+    # se le pasa una función que reciba el enveto de archivos seleccionados
+    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
+    
+    # se crea un cuadro de texto en la interfaz gráfica
+    carpeta_seleccionada = ft.Text()
+
+    page.overlay.append(pick_files_dialog)
+    
+    def renombrar(x: ft.ControlEvent):
+        print("Renombrando imágenes en: " + carpeta_seleccionada.value)
+        renombrar_imagenes.renombrar_imagenes(carpeta_seleccionada.value)
+        print("Listo, imágenes renombradas en: " + carpeta_seleccionada.value)
+
+    page.add(
+        ft.Row(
+            [
+                ft.ElevatedButton(
+                    "Seleccionar archivos",
+                    icon=ft.icons.UPLOAD_FILE,
+                    on_click=lambda _: pick_files_dialog.get_directory_path(),
+                ),
+                carpeta_seleccionada,
+                ft.FilledButton("Renombrar", icon=ft.icons.PLAY_CIRCLE_FILL, on_click=renombrar),
+            ]
+        )
+    )
+
+ft.app(main)
